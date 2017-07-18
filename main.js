a92758ae6d16f4266a7d4c1d29b942a4553c0b57
Xiaofei <xifzhang@thoughtworks.com>
2017-07-18 13:27:45 +0800

    change default port

--- main.js
+++ main.js
@@ -10,6 +10,6 @@ app.use('*', function (req, res) {
   res.sendFile(file);
 });
 
-app.listen(3000, function () {
-  console.log("Listening on port 3000!");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port %d!", this.address().port);
 });
